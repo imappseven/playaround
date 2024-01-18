94b2e06aa3bdc1d78228f07d5cf8b3b459329fde
m <m@ms-MacBook-Pro.local>
2024-01-18 21:23:57 +0700

    change the first solution

--- uniquesoccurence.py
+++ uniquesoccurence.py
@@ -5,14 +5,19 @@ class Solution(object):
         :rtype: bool
         """
         counts = {}
-        for e in arr:
-            if e in counts:
-                counts[e] += 1
-            else:
-                counts[e] = 1
+        if len(arr) == 1:
+            return True
+        else:
+            for e in arr:
+                if e in counts:
+                    counts[e] += 1
+                else:
+                    counts[e] = 1
+            
+        my_set = set(list(counts.values()))
+        if len(my_set) == len(list(counts.values())):
+            return True
+        else:
+            return False
 
-        for val in counts.values():
-            if list(counts.values()).count(val) > 1:
-                return False
-            else:
-                return True
+                
